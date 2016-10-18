8bba8228fd707b86ffab35746c59f2553a14c341
vehansayvazi <vehansayvazi5@gmail.com>
2016-10-18 16:20:03 -0700

    Connected to db and inserted an int

--- Fast_Brain.py
+++ Fast_Brain.py
@@ -1,31 +1,39 @@
+#1) Testing the "Fast_Brain" connection to db
+#2) inputing an int
+#
+#DATABASE SCHEMA
+#Database => Prime-Time-Project
+#Table => UserInput
+#Column=> num
+#
+
 import mysql.connector
 from mysql.connector import errorcode
 
+#First we must connect to the db and give error messages when we hit issues
 try:
     key = mysql.connector.connect(
-        user='root',
+        user='root', #MySQL user name
         password='1991', #my connection password
         host='localhost',
-        database='Prime-Time-Project' #this is the db name
-    )
-    print ("Connected to Database!")
+        database='Prime-Time-Project')
+    print ("Connected to Database!") #success
 
-except mysql.connector.Error as e:
-    if e.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with the username or password")
-    elif e.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database doesn't exist")
-    else:
-        print(e)
-cursor = key.cursor()
-addName =("INSERT INTO Name (fName, lName) VALUES (%s, %s)")
-fName="Vehans"
-lName="Ayvazi"
+except mysql.connector.Error as e: #fail ... what's the issue?
+   if e.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+       print("Something is wrong with the username or password")
+   elif e.errno == errorcode.ER_BAD_DB_ERROR:
+       print("Database doesn't exist")
+   else:
+       print(e)
 
-empName=(fName, lName)
+mycursor = key.cursor()
 
-cursor.execute(addName, empName)
+addNum = ("INSERT INTO UserInput (num) VALUES (%(x)s)") #sql command to insert x
+X = {'x' : 57} # used a dic. incase we want to input multi. values
+
+mycursor.execute(addNum, X)
 
 key.commit()
-cursor.close()
-key.close #
+mycursor.close()
+key.close
