41fbe3b47e3288d9cfbae462798808d9ca89b75e
vehansayvazi <vehansayvazi5@gmail.com>
2016-10-19 11:53:31 -0700

    mod to test db added print function

--- DB_Test.py
+++ DB_Test.py
@@ -1,7 +1,16 @@
 import Fast_Brain
 import Slow_Brain
 
+#testing all db related functions
+
+
 print("TEST_START - Processing: Testing DB input...")
-x = 5 #input
+x = 8 #test input
 Fast_Brain.DB_Input(x);
 print("TEST_END - Success: Input test passed")
+
+print("TEST_START - Processing: Testing DB print...")
+Fast_Brain.DB_PrintAll();
+print("TEST_END - Success: Print test passed")
+
+#will add more
--- Fast_Brain.py
+++ Fast_Brain.py
@@ -2,14 +2,14 @@
 #2) inputing an int
 #
 #DATABASE SCHEMA
-#Database => Prime-Time-Project
-#Table => UserInput
-#Column=> num
+#Database => TEST_DB
+#Table => test_table
+#Column=> a
 #
 
 import mysql.connector
 from mysql.connector import errorcode
-
+#database_name = 'Prime-Time-Project'
 
 def DB_Connect():
     #this function will connect us to the db and return the key for later use
@@ -18,7 +18,7 @@ def DB_Connect():
             user='root',  # MySQL user name
             password='1991',  # my connection password
             host='localhost',
-            database='Prime-Time-Project')
+            database='TEST_DB')
         print ("  Success: Connected to Database")  # success
     except mysql.connector.Error as e:  # fail ... what's the issue?
         if e.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -36,11 +36,10 @@ def DB_Input (num):
     mycursor = key.cursor()
 
     print ("  Processing: Inserting number into Database...")
-    addNum = ("INSERT INTO UserInput (num) VALUES (%(x)s)") #sql command to insert x
+    addNum = ("INSERT INTO test_table (a) VALUES (%(x)s)") #sql command to insert x
     X = {'x' : num} # used a dic. incase we want to input multi. values
-    print ("  Success: Number inserted into Database")
-
     mycursor.execute(addNum, X)
+    print ("  Success: Number inserted into Database")
 
     key.commit()
     mycursor.close()
@@ -48,6 +47,25 @@ def DB_Input (num):
     return;
 
 def DB_PrintAll ():
+    #to print all
+    key = DB_Connect()
+    mycursor = key.cursor()
 
+    print ("  Processing: Printing...")
 
-    return
+    mycursor.execute("USE TEST_DB")  # select the database
+    mycursor.execute("SELECT a from test_table")  # execute 'SHOW TABLES' (but data is not returned)
+
+    #tables = mycursor.fetchall()  # return data from last query
+
+    for (test_table) in mycursor:
+        print(test_table)
+
+    print ("  Success: Printed")
+
+    key.commit()
+    mycursor.close()
+    key.close
+    return;
+
+#will add more
