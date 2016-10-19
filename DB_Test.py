bf669b04b531b605b1376806030e3b33cb199115
vehansayvazi <vehansayvazi5@gmail.com>
2016-10-19 10:56:48 -0700

    Mod. to function based Added some test functions

---
+++ DB_Test.py
@@ -0,0 +1,7 @@
+import Fast_Brain
+import Slow_Brain
+
+print("TEST_START - Processing: Testing DB input...")
+x = 5 #input
+Fast_Brain.DB_Input(x);
+print("TEST_END - Success: Input test passed")
--- Fast_Brain.py
+++ Fast_Brain.py
@@ -10,30 +10,44 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-#First we must connect to the db and give error messages when we hit issues
-try:
-    key = mysql.connector.connect(
-        user='root', #MySQL user name
-        password='1991', #my connection password
-        host='localhost',
-        database='Prime-Time-Project')
-    print ("Connected to Database!") #success
 
-except mysql.connector.Error as e: #fail ... what's the issue?
-   if e.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-       print("Something is wrong with the username or password")
-   elif e.errno == errorcode.ER_BAD_DB_ERROR:
-       print("Database doesn't exist")
-   else:
-       print(e)
+def DB_Connect():
+    #this function will connect us to the db and return the key for later use
+    try:
+        key = mysql.connector.connect(
+            user='root',  # MySQL user name
+            password='1991',  # my connection password
+            host='localhost',
+            database='Prime-Time-Project')
+        print ("  Success: Connected to Database")  # success
+    except mysql.connector.Error as e:  # fail ... what's the issue?
+        if e.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("  Something is wrong with the username or password")
+        elif e.errno == errorcode.ER_BAD_DB_ERROR:
+            print("  Database doesn't exist")
+        else:
+            print(e)
+    return key;
 
-mycursor = key.cursor()
+def DB_Input (num):
+    # First we must connect to the db and give error messages when we hit issues
+    key = DB_Connect()
 
-addNum = ("INSERT INTO UserInput (num) VALUES (%(x)s)") #sql command to insert x
-X = {'x' : 57} # used a dic. incase we want to input multi. values
+    mycursor = key.cursor()
 
-mycursor.execute(addNum, X)
+    print ("  Processing: Inserting number into Database...")
+    addNum = ("INSERT INTO UserInput (num) VALUES (%(x)s)") #sql command to insert x
+    X = {'x' : num} # used a dic. incase we want to input multi. values
+    print ("  Success: Number inserted into Database")
 
-key.commit()
-mycursor.close()
-key.close
+    mycursor.execute(addNum, X)
+
+    key.commit()
+    mycursor.close()
+    key.close
+    return;
+
+def DB_PrintAll ():
+
+
+    return
