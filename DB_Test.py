598aa57a40ffc888f076725690b2185456e22f98
vehansayvazi <vehansayvazi5@gmail.com>
2016-11-05 09:31:27 -0700

    object oriented design ... still working on the test files I'll need to change a few files around still

--- DB_Test.py
+++ DB_Test.py
@@ -1,15 +1,15 @@
-import utility
+import Utility
 
 #testing all db related functions
 
 
 print("TEST_START - Processing: Testing DB input...")
 x = 8 #test input
-Fast_Brain.DB_Input(x);
+Utility.DB_Input(x);
 print("TEST_END - Success: Input test passed")
 
 print("TEST_START - Processing: Testing DB print...")
-Fast_Brain.DB_PrintAll();
+Utility.DB_PrintAll();
 print("TEST_END - Success: Print test passed")
 
 #will add more
