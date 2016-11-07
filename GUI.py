e3303b645bfbd63d401fe39f66c8028f41623134
i-abdi <i.abdi@hotmail.com>
2016-11-07 10:04:01 -0800

    some test code, this has hwo to add buttons and configure currently.

--- GUI.py
+++ GUI.py
@@ -1,6 +1,28 @@
 # Project Name: Prime-Time-Project
 # File: GUI.py
 # NOTE: this is our graphical user interface
+from tkinter import *  # Importing the Tkinter (tool box) library
+
+root = Tk()  # Creats object root that has properties for the window. Access via .instr
+
+# configuration portion
+root.font = ('Verdana', '20', 'bold')   #changes the font for ALL text belonging to root
+
+def pfunction():
+    pass  # some code here hitting yes, pass just means no code is running here
+    return
+
+
+def npfunction():
+    pass  # just pass this function for now
+    return
+
+
+Ybutton = Button(root, text="Prime", command=pfunction(), fg='blue', bg='green',).pack()            #currently packed just to populate the message box
+Nbutton = Button(root, text="Not-Prime", command=npfunction(), fg='black', bg='red').pack()         #need to link to functions
+                                                                                                    #above for functionallity
+
+root.mainloop()  # Execute the main event handler
 
 import index
 from _tkinter import *
