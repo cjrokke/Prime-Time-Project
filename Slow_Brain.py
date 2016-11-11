e2c93fb636fc479e1fd99dd5db2e71b23e921015
vehansayvazi <vehansayvazi5@gmail.com>
2016-11-11 13:59:23 -0800

    implemented a object oriented design for the slow brain

--- Slow_Brain.py
+++ Slow_Brain.py
@@ -2,18 +2,34 @@ from Database import Database
 #import Fast_Brain
 import math
 
-def prime(number):
-    if number % 2 == 0: #Is the number even?
-        return False
-    else:
-        x = math.sqrt(number) #Find the ranges max for checking prime
-        x = int(x) #rounds number down
-        z = 0.0 #Needs to be decimal so python doesn't round to zero
-        for i in range(3, x):
-            z = number % i
-            if z == 0: #If a number divides evenly 
-                return False #Not Prime
-            if i + 1 == x: #Has it checked against all number?
-                return True #Prime
+class SlowBrain(object):
+    def __init__(self):
+        self.number = None
 
-print(prime(17))
+    def primeDetermination(self, d):
+        self.number = d
+        if self.number % 2 == 0: # Is the number even?
+            #print ("not prime (its even)")
+            return False
+        else:
+            x = math.sqrt(self.number) # Find the ranges max for checking prime
+            x = int(x) # rounds number down
+            z = 0.0 # Needs to be decimal so python doesn't round to zero
+            for i in range(3, x):
+                z = self.number % i
+                if z == 0: # If a number divides evenly
+                    #print ("not prime")
+                    return False # Not Prime
+                if i + 1 == x: # Has it checked against all number?
+                    #print ("prime")
+                    return True # Prime
+
+
+
+
+
+
+#testing
+#a = SlowBrain()
+#print(a.primeDetermination(13))
+#a.primeDetermination(13)
