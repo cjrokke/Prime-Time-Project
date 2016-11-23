a9d02c0362b37d1141c4608de54c728083005c99
i-abdi <i.abdi@hotmail.com>
2016-11-23 11:39:16 -0800

    added gridding

--- GUI.py
+++ GUI.py
@@ -1,29 +1,30 @@
 # Project Name: Prime-Time-Project
 # File: GUI.py
 # NOTE: this is our graphical user interface
-# import index
+import index
 from Tkinter import *  # Importing the Tkinter (tool box) library
 # might need be "tkinter"
-#from Slow_Brain import *
-#from Fast_Brain import *
-
-# add import brain
-
-
-def primeCheck(event):
-    # if prime bool true
-    # flash button using fg bg
-    # else flash button using fg bg
-    # refresh FB time sb time
-    pass
+from Slow_Brain import *
+from Fast_Brain import *
+ add import brain
 
 
-def sbTime():
-    return "yeo"  # placeholder string
+ def primeCheck(event):
 
-
-def fbTime():
-    return "test"  # placeholder string
+     entry1.get()
+#     # if prime bool true
+#     # flash button using fg bg
+#     # else flash button using fg bg
+#     # refresh FB time sb time
+#     pass
+#
+#
+# def sbTime():
+#     return "yeo"  # placeholder string
+#
+#
+# def fbTime():
+#     return "test"  # placeholder string
 
 root = Tk()  # Creats object root that has properties for the window. Access via .instr
 # configuration portion
@@ -31,12 +32,14 @@ root.font = ('Verdana', '20', 'bold')  # changes the font for ALL text belonging
 
 dummyEvent = FALSE
 
-entry1 = Entry(root, text="test").pack
-SBtime = Label(root, text="test").pack
-FBtime = Label(root, text="test").pack
-PLabel = Label(root, text="Placeholder Prime/Notprime", font="Times 19 bold").pack
-Ybutton = Button(root, text="Prime", fg='blue', bg='green').pack()  # currently packed just to populate the message box
-Nbutton = Button(root, text="Not-Prime", fg='black', bg='red').pack()  # need to link to functions
+SBtime = Label(root, text="test1").grid(row=1, sticky = W)
+FBtime = Label(root, text="test2").grid(row=1,sticky = E)
 
+entry1 = Entry(root, text="test3").grid(row=2)
+PLabel = Label(root, text="Enter Value to Check", font="Times 10").grid(row=3)
+Ybutton = Button(root, text="Prime", fg='blue', bg='green').grid(row=0,  sticky=W)  # currently packed just to populate the message box
+Nbutton = Button(root, text="Not-Prime", fg='black', bg='red').grid(row=0, sticky=E)# need to link to functions
+
+entry1.bind("<return>",primeCheck)
 
 root.mainloop()  # Execute the main event handler
