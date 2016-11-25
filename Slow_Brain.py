8f7983867bce1b16a1e5791cae69ee4fe05a667d
cjrokke <cjrokke@yahoo.com>
2016-11-25 12:57:06 -0800

    Update Slow_Brain.py
    Split into 2 modes. User input mode spits out an answer faster and doesnt log anything. Generated mode tests exhaustively and logs the time as well as a list of divisors to update the probability if we build it in.
    
    Time logging in generated mode is intentionally slowed down for comparison with our slow FB...

--- Slow_Brain.py
+++ Slow_Brain.py
@@ -1,5 +1,5 @@
-from __future__ import print_function
 from Database import Database
+# import Fast_Brain
 import math
 import time
 
@@ -10,40 +10,69 @@ database = 'TEST_DB'
 
 class SlowBrain(object):
     def __init__(self):
-        self.number = None
+        self.input = None
+        self.genFlag = None
+        self.divisors = []
         self.SBtime = 0.00
 
-    def SBPrimeDetermination(self, d):
-        self.number = d
+    def SBPrimeDetermination(self, d, f):
+        self.input = d
+        self.genFlag = f
+        print'Starting up Slow Brain...'
+        if(self.genFlag): self.exhaustiveAlg(self)  #Input from SNG
+        else: self.fastAlg(self)    #Input form User
+
+    def exhaustiveAlg(self):
         db = Database(user, password, host, database)
-
-        print("Starting up Slow Brain...")
-
         start = time.time()
-
-        if self.number % 2 == 0: # Is the number even?
-            #print ("not prime (its even)")
-            return False
+        #end = start   #used for logging faster SB times
+        self.Divisors = []  # Divisor list
+        z = 0.000000000000  # for testing divisors
+        if (self.input == 2): return True  # 2 is Prime
+        if (self.input == 3): return True  # 3 is Prime
         else:
-            x = math.sqrt(self.number) # Find the ranges max for checking prime
-            x = int(x)+1 # rounds number down
-            z = 0.0 # Needs to be decimal so python doesn't round to zero
-            for i in range(3, x, 2):
-                z = self.number % i
-                if z == 0: # If a number divides evenly
-                    end = time.time()
-                    self.SBtime = end - start
-                    print("The SB search took ", str(self.SBtime), " seconds.")
-                    print ("Slow Brain: ===================> [ not prime ]")
-                    return False # Not Prime
+            for i in range(1, (self.input / 2) + 1):
+                for j in range(0, 2):               # test all divisors using nested for loops to prevent memory overflow for large inputs
+                    if (i != self.input / 2 | j != 1):  # neglect last result, input will always be divisible by input and never by input-1
+                        z = self.input % (2 * i + j)
+                        if (z == 0 & (j == 1 | i == 1)):    #Only store odds > 2
+                            #if (end == start): end = time.time()      #faster slow brain time logging
+                            self.divisors.append(2 * i + j)  #NotPrime
+            if (len(self.divisors) == 0):  # Prime!
+                end = time.time()
+                self.SBtime = end - start
+                db.StorePrimeSBtime(self.input, self.SBtime)   #Prime!
+                return True  # Prime!
+            else:  # NotPrime
+                #db.UpdateProb(self.divisors)
+                for i in range(0, len(self.divisors)): print self.divisors[i]
+                return False  # NotPrime
 
-            end = time.time()
-            self.SBtime = end - start
-            print ("Slow Brain: ===================> [ prime ]")
-            print("The SB search took ", str(self.SBtime), " seconds.")
-            db.StorePrimeSBtime(self.number,self.SBtime)
-            return True # Prime
+    def fastAlg(self):
+        if (self.input <= 1):
+            print'Invalid Format, please input a positive number > 1!'
+            return False  # Anything less than 1 cannot be tested and thus is NotPrime by definition #
+        if (self.input != int(self.input)):
+            print'Invalid Format, please input an integer!'
+            return False  # Non-integers cannot be tested and thus are NotPrime by definition
+        if (self.input == 2): return True  # 2 is Prime
+        if (self.input == 3): return True  # 3 is Prime
+        else:  # the 3 primes and 2 conditions above cannot be accurately tested using the method below
+            if (self.input % 2 == 0): return False  # even numbers = NotPrime
+            else:  # Algorithm
+                x = math.sqrt(self.input)  # Find the ranges max for checking prime
+                if (x == int(x)): return False  # square root whole = NotPrime
+                else:
+                    x = int(x) + 1  # rounds square root to next whole number up
+                    # print(x)
+                    z = 0.000000000000  # Needs to be decimal so python doesn't round to zero
+                    for i in range(2, x):
+                        z = self.input % i
+                        if (z == 0):  # If a number divides evenly
+                            print'Your input was divisible by: ', self.input / i, ' and ', i
+                            return False  # Not Prime
+                        if (i + 1 == x): return True  #Has it checked against all numbers? → Prime
 
-#testing
-# a = SlowBrain()
-# print("here" + str(a.SBPrimeDetermination(88)))
+# testing
+a = SlowBrain()
+print("here" + str(a.SBPrimeDetermination(88, 1)))
